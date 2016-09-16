824b898dc8155d656122a937b92b1134bc479087
teerstudio <ddz_ttt@hotmail.com>
2016-09-17 02:19:46 +0700

    ssss

--- index.php
+++ index.php
@@ -142,10 +142,11 @@
         }
 
         // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-            this.setZoom(14);
-            google.maps.event.removeListener(boundsListener);
-        });
+        
+        // var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        //     this.setZoom(14);
+        //     google.maps.event.removeListener(boundsListener);
+        // });
 
 
       }

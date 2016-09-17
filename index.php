5c51b0ff71e0894f5b7088795d731db4fbcf55aa
Thirawat Phannet <ddz_ttt@hotmail.com>
2016-09-17 10:43:54 +0700

    Update page title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Provider Map</title>
+    <title>Phuket Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
     <style>

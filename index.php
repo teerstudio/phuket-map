bdd8128c0db3145875e83840757b7696e6612613
Thirawat Phannet <ddz_ttt@hotmail.com>
2016-09-23 12:20:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,57 +40,57 @@
         // Multiple Markers
         var markers = [
 
-['	37 Thalang Guest House	',	7.88477	,	98.3883	], // Oldtown
-['	43 Guesthouse	',	7.88412	,	98.3895	], // Oldtown
-['	99 Oldtown Boutique Guesthouse	',	7.88477	,	98.3897	], // Oldtown
-['	A dessert moments	',	7.88486	,	98.3876	], // Oldtown
-['	Ai Phuket Hostel	',	7.88585	,	98.3876	], // Oldtown
-['	Fulfill Hostel Phuket	',	7.883	,	98.39	], // Oldtown
-['	Gallery Cafe	',	7.884094	,	98.3875	], // Oldtown
-['	Kopi De Phuket	',	7.882331	,	98.391247	], // Oldtown
-['	Pancake Corner	',	7.88412	,	98.3895	], // Oldtown
-['	Sino Imperial Phuket	',	7.882632	,	98.391199	], // Oldtown
-['	The Rommanee Classic Guesthouse	',	7.88504	,	98.3892	], // Oldtown
-['	Unforgettable	',	7.88352	,	98.3874	], // Oldtown
-['	Kopitiam	',	7.88474	,	98.3874	], // Oldtown
-['	Tom Yum Kung Restaurant	',	7.8837	,	98.3879	], // Oldtown
-['	Taitong Café	',	7.88477	,	98.3889	], // Oldtown
-['	Tukabkhao	',	7.88451	,	98.3867	], // Oldtown
-['	Phu Thalang Shop 	',	7.884869	,	98.38762	], // Oldtown
+['	37 Thalang Guest House	',	7.8849301	,	98.388254	], // Oldtown
+['	43 Guesthouse	',	7.8849301	,	98.3885146	], // Oldtown
+['	99 Oldtown Boutique Guesthouse	',	7.8848371	,	98.3896841	], // Oldtown
+['	A dessert moments	',	7.884806	,	98.387706	], // Oldtown
+['	Ai Phuket Hostel	',	7.885935	,	98.387764	], // Oldtown
+['	Fulfill Hostel Phuket	',	7.883078	,	98.390205	], // Oldtown
+['	Gallery Cafe	',	7.883879	,	98.387324	], // Oldtown
+['	Kopi De Phuket	',	7.882323	,	98.391246	], // Oldtown
+['	Pancake Corner	',	7.883002	,	98.390721	], // Oldtown
+['	Sino Imperial Phuket	',	7.882621	,	98.391143	], // Oldtown
+['	The Rommanee Classic Guesthouse	',	7.884958	,	98.38737	], // Oldtown
+['	Unforgettable	',	7.88355	,	98.387486	], // Oldtown
+['	Kopitiam	',	7.884752	,	98.388092	], // Oldtown
+['	Tom Yum Kung Restaurant	',	7.812637	,	98.298554	], // Oldtown
+['	Taitong Café	',	7.884746	,	98.388245	], // Oldtown
+['	Tukabkhao	',	7.883679	,	98.387952	], // Oldtown
+['	Phu Thalang Shop 	',	7.884931	,	98.388254	], // Oldtown
 ['	Go Tum	',	7.88469	,	98.3887	], // Oldtown
-['	Club No.43	',	7.88451	,	98.3867	], // Oldtown
-['	Glass House Restaurant	',	7.88544	,	98.3912	], // Oldtown
-['	The Tint @ Phuket Town	',	7.88661	,	98.3924	], // Oldtown
-['	AMA Phuket Hostel	',	7.88544	,	98.3912	], // Oldtown
-['	The Memory at On On	',	7.88546	,	98.3902	], // Oldtown
-['	Macchiato House	',	7.8847584	,	98.3858809	], // Oldtown
-['	Coffs & Burgh	',	7.88546	,	98.3902	], // Oldtown
+['	Club No.43	',	7.884366	,	98.387383	], // Oldtown
+['	Glass House Restaurant	',	7.886475	,	98.393309	], // Oldtown
+['	The Tint @ Phuket Town	',	7.886691	,	98.392281	], // Oldtown
+['	AMA Phuket Hostel	',	7.884793	,	98.392856	], // Oldtown
+['	The Memory at On On	',	7.884008	,	98.388488	], // Oldtown
+['	Macchiato House	',	7.88476	,	98.388069	], // Oldtown
+['	Coffs & Burgh	',	7.884692	,	98.389667	], // Oldtown
 
-['	Baan Laimai Beach Resort	',	7.88979	,	98.2931	], // Patong
-['	Eurasian Restaurant	',	7.89307	,	98.2957	], // Patong
-['	Heaven Spa & Massage	',	7.89331	,	98.2979	], // Patong
-['	Koolbackpacker Hostel	',	7.8934	,	98.2975	], // Patong
-['	Patong Backpacker	',	7.89496	,	98.2958	], // Patong
-['	Patong Beach Club	',	7.89253	,	98.2946	], // Patong
-['	Patong Boutique Hotel	',	7.8933	,	98.295	], // Patong
-['	Red Hot club	',	7.89308	,	98.2983	], // Patong
-['	Relaxing Massage	',	7.89298	,	98.297	], // Patong
-['	Sabai Sabai Restaurant Patong	',	7.891593	,	98.294109	], // Patong
-['	Savoey Restaurant	',	7.89406	,	98.2953	], // Patong
-['	R-Mar Resort and Spa	',	7.896244	,	98.298663	], // Patong
-['	The Royal Plam Beach	',	7.88949	,	98.2929	], // Patong
-['	Royal Baan Kamnan Restaurant	',	7.894836	,	98.295772	], // Patong
-['	Pimnara Spa	',	7.89287	,	98.2947	], // Patong
-['	Elle Bar Bistro Bed	',	7.89041	,	98.294	], // Patong
-['	Elephantgod Tattooart	',	7.89149	,	98.2942	], // Patong
-['	Orange Restaurant	',	7.89857	,	98.2976	], // Patong
-['	The Orchids	',	7.890024	,	98.2921405	], // Patong
-['	The Sweet Restaurant	',	7.89161	,	98.2948	], // Patong
-['	Passorn Massage	',	7.8954	,	98.2962	], // Patong
-['	Siam Healthland Phuket	',	7.89018	,	98.2933	], // Patong
-['	Tiras Patong Beach Phuket	',	7.8933	,	98.295	], // Patong
-['	Patong Rose Guest House	',	7.89016	,	98.294	], // Patong
-['	Andaman Embrace Patong	',	7.90013	,	98.2967	] // Patong
+['	Baan Laimai Beach Resort	',	7.889843, 	,	98.293153	], // Patong
+['	Eurasian Restaurant	',	7.893395, 	,	98.296639	], // Patong
+['	Heaven Spa & Massage	',	7.893146, 	,	98.298049	], // Patong
+['	Koolbackpacker Hostel	',	7.893413, 	,	98.298028	], // Patong
+['	Patong Backpacker	',	7.894805, 	,	98.295777	], // Patong
+['	Patong Beach Club	',	7.89253	,	98.294521	], // Patong
+['	Patong Boutique Hotel	',	7.893331	,	98.294886	], // Patong
+['	Red Hot club	',		,		], // Patong
+['	Relaxing Massage	',		,		], // Patong
+['	Sabai Sabai Restaurant Patong	',		,		], // Patong
+['	Savoey Restaurant	',	7.894465	,	98.295714	], // Patong
+['	R-Mar Resort and Spa	',	7.895477	,	98.297893	], // Patong
+['	The Royal Plam Beach	',		,		], // Patong
+['	Royal Baan Kamnan Restaurant	',	7.894812	,	98.296426	], // Patong
+['	Pimnara Spa	',	7.8913569	,	98.2948953	], // Patong
+['	Ella Bar Bistro Bed	',	7.891408,	,	98.294875	], // Patong
+['	Elephantgod Tattooart	',		,		], // Patong
+['	Orange Restaurant	',	7.8962262	,	98.298225	], // Patong
+['	The Orchids	',	7.8900239	,	98.294329	], // Patong
+['	The Sweet Restaurant	',	7.891611	,	98.294953	], // Patong
+['	Passorn Massage	',	7.895222	,	98.295986	], // Patong
+['	Siam Healthland Phuket	',		,		], // Patong
+['	Tiras Patong Beach Phuket	',	7.890438	,	98.293942	], // Patong
+['	Patong Rose Guest House	',	7.890158	,	98.294093	], // Patong
+['	Andaman Embrace Patong	',	7.898991	,	98.29896	] // Patong
 
 
         ];
